2d38e85c5a5e1b51a5644420180474cd51ff1da3
jack-chapman <jackchapman093@gmail.com>
2022-05-15 21:52:56 +0100

    Add title, description, and lang

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
 <template>
+  <Html lang="en"></Html>
+  <Head>
+    <Title>jackchapman.dev</Title>
+    <Meta name="description" content="Personal website for Jack Chapman" />
+  </Head>
   <div class="grid place-items-center bg-clay h-screen">
     <div class="grid grid-cols-[repeat(3,400px)] grid-rows-2 gap-2">
       <NameModule />
