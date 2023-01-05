e145eaa37f08cecb1ac7f2396e716e3c2c77ef00
jack-chapman <jackchapman093@gmail.com>
2023-01-05 20:56:33 +0000

    Add vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['nuxt-windicss'],
+  plugins: [
+    {
+      src: '~/plugins/vercel',
+      mode: 'client',
+    },
+  ],
 });
