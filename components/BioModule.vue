6ced67dbb74454be1019b3af5289f894b795d428
jack-chapman <jackchapman093@gmail.com>
2024-04-03 19:33:11 +0100

    Update workplace and deps

--- components/BioModule.vue
+++ components/BioModule.vue
@@ -4,6 +4,7 @@
     A <span class="text-cloud">front-end developer</span> based in
     <span class="text-cloud">Brighton, UK</span>.<br />
     Currently working at
-    <a class="text-blush hover:underline" href="https://tillo.io">Tillo</a>.
+    <a class="text-blush hover:underline" href="https://about.gitlab.com" target="_blank"
+      rel="noopener noreferrer nofollow">GitLab</a>.
   </ModuleBase>
 </template>
--- components/LetterColumn.vue
+++ components/LetterColumn.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="overflow-hidden h-60 lg:h-75 relative w-full">
     <ul class="absolute left-0 custom-animation w-full">
-      <li
-        v-for="(char, i) of fullList"
-        :key="i"
-        :class="bg()"
-        class="text-pale h-20 lg:h-25 font-mono list-none grid place-items-center w-full border border-pale transition-colors"
-      >
+      <li v-for="(char, i) of fullList" :key="i" :class="bg()"
+        class="text-pale h-20 lg:h-25 font-mono list-none grid place-items-center w-full border border-pale transition-colors">
         <h2 class="text-4xl">
           {{ char }}
         </h2>
@@ -16,8 +12,8 @@
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
-import { random } from 'lodash-es';
+import type { PropType } from "vue";
+import { random } from "lodash-es";
 
 const props = defineProps({
   chars: {
@@ -32,9 +28,9 @@ const fullList = ref([...props.chars, ...props.chars, ...props.chars]);
 const bg = () => {
   const num = [1, 2, 3][Math.floor(random(0, 2))];
   return {
-    'hover:(bg-blush text-clay)': num === 3,
-    'hover:(bg-cloud text-clay)': num === 2,
-    'hover:(bg-pale text-clay)': num === 1,
+    "hover:(bg-blush text-clay)": num === 3,
+    "hover:(bg-cloud text-clay)": num === 2,
+    "hover:(bg-pale text-clay)": num === 1,
   };
 };
 </script>
@@ -45,10 +41,12 @@ const bg = () => {
   animation-duration: 10s;
   animation-iteration-count: infinite;
   animation-delay: v-bind(delay);
+
   @media (min-width: 1024px) {
     animation-name: slidein;
   }
 }
+
 @keyframes slidein {
   0% {
     bottom: 0px;
@@ -62,6 +60,7 @@ const bg = () => {
     bottom: -600px;
   }
 }
+
 @keyframes slidein-small {
   0% {
     bottom: 0px;
